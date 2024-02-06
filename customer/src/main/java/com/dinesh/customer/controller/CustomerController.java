a9e822400681c0e5522a0ec9ec3f117986839f31
Dinesh Reddy <dineshmaareddy@gmail.com>
2024-02-06 10:01:45 +0530

    moved uris to application.properties

--- customer/src/main/java/com/dinesh/customer/controller/CustomerController.java
+++ customer/src/main/java/com/dinesh/customer/controller/CustomerController.java
@@ -7,6 +7,7 @@ import com.dinesh.customer.service.CustomerService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +17,13 @@ import java.util.List;
 @CrossOrigin
 public class CustomerController {
     private static final Log LOGGER = LogFactory.getLog(CustomerController.class);
+
+    @Value("${plan.uri}")
+    String planUri;
+
+
+    @Value("${friend.uri}")
+    String friendUri;
 
     @Autowired
     private CustomerService customerService;
@@ -36,9 +44,9 @@ public class CustomerController {
     public CustomerDTO getCustomerProfile(@PathVariable Long phoneNo) {
         LOGGER.info("Profile request for customer " + phoneNo);
         CustomerDTO customerDTO = customerService.getCustomerProfile(phoneNo);
-        PlanDTO planDTO = new RestTemplate().getForObject("http://localhost:9400/plans/" + customerDTO.getCurrentPlan().getPlanId(), PlanDTO.class);
+        PlanDTO planDTO = new RestTemplate().getForObject(planUri + customerDTO.getCurrentPlan().getPlanId(), PlanDTO.class);
         customerDTO.setCurrentPlan(planDTO);
-        List<Long> friendsAndFamily = new RestTemplate().getForObject("http://localhost:9300/customers/"+customerDTO.getPhoneNo()+"/friends", List.class);
+        List<Long> friendsAndFamily = new RestTemplate().getForObject(friendUri+customerDTO.getPhoneNo()+"/friends", List.class);
         customerDTO.setFriendAndFamily(friendsAndFamily);
         return customerDTO;
     }
